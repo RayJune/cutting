fdf4d5eb66160cd60da1d88fdbb29bbda32311fe
RayJune <rayjune.x@gmail.com>
2021-09-13 12:25:30 +0800

    test(linked-list): use buildListNode function to optimize test code

--- src/linked-list/merge-two-sorted-lists/test.js
+++ src/linked-list/merge-two-sorted-lists/test.js
@@ -4,24 +4,33 @@ const mergeTwoLists = require('./optimize');
 // const mergeTwoLists = require('./template');
 // const mergeTwoLists = require('./template-zh');
 
+/**
+ *
+ * @param {Array} arr
+ * @returns {ListNode}
+ */
+function buildListNode(arr) {
+    let preHead = new ListNode(-1);
+    let node = preHead;
+
+    arr.forEach(num => {
+        node.next = new ListNode(num);
+        node = node.next;
+    });
+
+    return preHead.next;
+}
+
 test('define mergeTwoLists function', () => {
     expect(typeof mergeTwoLists).toBe('function');
 });
 
 test('l1 = [1, 2, 4], l2 = [1, 3, 4]', () => {
-    const l1 = new ListNode(1, new ListNode(2, new ListNode(4)));
-    const l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
-    const arr = [1, 1, 2, 3, 4, 4];
-    const preHead = new ListNode();
-    let preNode = preHead;
+    const l1 = buildListNode([1, 2, 4]);
+    const l2 = buildListNode([1, 3, 4]);
+    const l3 = buildListNode([1, 1, 2, 3, 4, 4]);
 
-    arr.forEach(num => {
-        preNode.next = new ListNode(num, null);
-        preNode = preNode.next;
-    });
-
-    expect(mergeTwoLists(l1, l2)).toEqual(preHead.next);
-    // expect(mergeTwoLists(l1, l2)).toEqual(new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(4)))))));
+    expect(mergeTwoLists(l1, l2)).toEqual(l3);
 });
 
 test('l1 = [], l2 = []', () => {
