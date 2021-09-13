06f8dc82cdcf2660a9f25d48ab9ec5a64209b8a8
RayJune <rayjune.x@gmail.com>
2021-09-13 16:18:28 +0800

    feat(linked-list): add add-two-numbers and its solution

---
+++ src/linked-list/add-two-numbers/template-zh.js
@@ -0,0 +1,38 @@
+/*
+ * 给你两个非空的链表，表示两个非负的整数。它们每位数字都是按照逆序的方式存储的，
+ * 并且每个节点只能存储一位数字。
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ *
+ * Example 1:
+ * Input: l1 = [2, 4, 3], l2 = [5, 6, 4]
+ * Output: [7, 0, 8]
+ * Explanation: 342 + 465 = 807.
+ *
+ * Example 2:
+ * Input: l1 = [0], l2 = [0]
+ * Output: [0]
+ *
+ * Example 3:
+ * Input: l1 = [9, 9, 9, 9, 9, 9, 9], l2 = [9, 9, 9, 9]
+ * Output: [8, 9, 9, 9, 0, 0, 0, 1]
+ *
+ * Constraints:
+ *
+ * 两个链表的节点数目范围是 [1, 100]
+ * 0 <= Node.val <= 9
+ * 题目数据保证列表表示的数字不含前导零
+ *
+ * https://leetcode-cn.com/problems/add-two-numbers/
+ *
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+module.exports = addTwoNumbers;
---
+++ src/linked-list/add-two-numbers/test.js
@@ -0,0 +1,58 @@
+const ListNode = require('./list-node');
+const addTwoNumbers = require('./index');
+// const addTwoNumbers = require('./template');
+// const addTwoNumbers = require('./template-zh');
+
+/**
+ *
+ * @param {Array} arr
+ * @returns {ListNode}
+ */
+function buildListNode(arr) {
+    let preHead = new ListNode(-1);
+    let node = preHead;
+
+    arr.forEach(num => {
+        node.next = new ListNode(num);
+        node = node.next;
+    });
+
+    return preHead.next;
+}
+
+test('define addTwoNumbers function', () => {
+    expect(typeof addTwoNumbers).toBe('function');
+});
+
+test('l1 = [2, 4, 3], l2 = [5, 6, 4]', () => {
+    const l1 = buildListNode([2, 4, 3]);
+    const l2 = buildListNode([5, 6, 4]);
+    const l3 = buildListNode([7, 0, 8]);
+
+    expect(addTwoNumbers(l1, l2)).toEqual(l3);
+});
+
+test('l1 = [0], l2 = [0]', () => {
+    const l1 = buildListNode([0]);
+    const l2 = buildListNode([0]);
+    const l3 = buildListNode([0]);
+
+    expect(addTwoNumbers(l1, l2)).toEqual(l3);
+});
+
+test('l1 = [9, 9, 9, 9, 9, 9, 9], l2 = [9, 9, 9, 9]', () => {
+    const l1 = buildListNode([9, 9, 9, 9, 9, 9, 9]);
+    const l2 = buildListNode([9, 9, 9, 9]);
+    const l3 = buildListNode([8, 9, 9, 9, 0, 0, 0, 1]);
+
+    expect(addTwoNumbers(l1, l2)).toEqual(l3);
+});
+
+
+test('l1 = [9, 9, 9, 9], l2 = [9, 9, 9, 9, 9, 9, 9]', () => {
+    const l1 = buildListNode([9, 9, 9, 9]);
+    const l2 = buildListNode([9, 9, 9, 9, 9, 9, 9]);
+    const l3 = buildListNode([8, 9, 9, 9, 0, 0, 0, 1]);
+
+    expect(addTwoNumbers(l1, l2)).toEqual(l3);
+});
