81a0c47e24fd3c265f4b7ff9d8e02ad76b1f579f
RayJune <rayjune.x@gmail.com>
2021-09-16 11:52:32 +0800

    refactor(linked-list): optimize reverse-linked-list-ii's optimize.js

--- src/linked-list/reverse-linked-list-ii/optimize.js
+++ src/linked-list/reverse-linked-list-ii/optimize.js
@@ -39,7 +39,7 @@ class ListNode {
 /**
  * 把中间的链表反转，再更改反转链表首尾的指向，注意细节的处理
 
- * Time Complexity: O(n) = while 循环以及反转链表的最大可能值是 n 次
+ * Time Complexity: O(n) = 两个 for 循环加一起的最大次数是 O(n)，即 left 为 1， right 为末尾时
  * Space complexity: O(1)
  * Auxiliary complexity: O(1)
  * n 代表输入参数 head 链表的长度
@@ -50,45 +50,34 @@ class ListNode {
  * @returns {ListNode}
  */
 function reverseBetween(head, left, right) {
-    let preNode = null;
-    let targetHead = head;
-    let targetTail = head;
-    // 先遍历找到要待反转区间之前前的节点和之后的节点，以备后面更改指向使用
-    // 同时也要找到待反转区间的头节点和尾节点，方便后续的反转以及反转后更改指向的处理
-    for (let i = 1, node = head; i < right; i++) {
-        const nextNode = node.next;
+    const preHead = new ListNode(-1, head);
+    let preNode = preHead;
 
-        if (i === left - 1) {
-            preNode = node;
-            preNode.next = null;
-            targetHead = nextNode;
-            targetTail = targetHead;
-        }
-        if (i > left - 1) {
-            targetTail = targetTail.next;
-        }
-        node = nextNode;
+    for (let i = 0; i < left - 1; i++) {
+        preNode = preNode.next;
     }
-    // 开始处理反转相关的逻辑
-    const afterNode = targetTail.next;
 
-    targetTail.next = null;
-    reverseLinkedList(targetHead);
-    // 反转完成后，原来的 targetHead 就成了尾部节点，targetTail 成为头部节点
-    if (preNode) {
-        preNode.next = targetTail;
-    } else {
-        head = targetTail;
+    let rightNode = preNode;
+
+    for (let i = 0; i < right - left + 1; i++) {
+        rightNode = rightNode.next;
     }
-    targetHead.next = afterNode;
 
-    return head;
+    const leftNode = preNode.next;
+    const afterNode = rightNode.next;
+
+    preNode.next = null;
+    rightNode.next = null;
+    reverseLinkedList(leftNode); // 注意反转后，原来的头结点和未节点会调转过来
+    preNode.next = rightNode;
+    leftNode.next = afterNode;
+
+    return preHead.next;
 }
 
 /**
  *
  * @param {ListNode} head
- * @returns {ListNode}
  */
 function reverseLinkedList(head) {
     let preNode = null;
@@ -100,8 +89,6 @@ function reverseLinkedList(head) {
         preNode = head;
         head = nextNode;
     }
-
-    return preNode;
 }
 
 module.exports = reverseBetween;
