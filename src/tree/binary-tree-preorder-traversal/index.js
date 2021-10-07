3144a15574b88e54804396137a5246347aceb7b2
RayJune <rayjune.x@gmail.com>
2021-10-07 18:33:05 +0800

    refactor(tree): update binary-tree-preorder-traversal's comments

--- src/tree/binary-tree-preorder-traversal/index.js
+++ src/tree/binary-tree-preorder-traversal/index.js
@@ -46,6 +46,7 @@
  * Time Complexity: O(n) = 遍历次数 O(n)
  * Space complexity: O(n) = result 数组长度 O(n) + 函数调用栈深度 O(n)
  * Auxiliary complexity: O(n) = 函数调用栈深度 O(n) 最坏情况下树呈现链状
+ * 其中 n 为二叉树的节点数
  *
  * @param {TreeNode} root
  * @param {number[]} result
