187e61f9e4b8897a5fda825bb7f4a0d2b9ac0447
RayJune <rayjune.x@gmail.com>
2021-12-05 08:56:35 +0800

    feat(dp): add 53-maximum-subarray

---
+++ src/dp/53-maximum-subarray/test.js
@@ -0,0 +1,23 @@
+const maxSubArray = require('./index');
+// const maxSubArray = require('./template');
+// const maxSubArray = require('./template-zh');
+
+test('define maxSubArray function', () => {
+    expect(typeof maxSubArray).toBe('function');
+});
+
+test('nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]', () => {
+    const nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+
+    expect(maxSubArray(nums)).toBe(6);
+});
+
+test('nums = [1]', () => {
+    expect(maxSubArray([1])).toBe(1);
+});
+
+test('nums = [5, 4, -1, 7, 8]', () => {
+    const nums = [5, 4, -1, 7, 8];
+
+    expect(maxSubArray(nums)).toBe(23);
+});
