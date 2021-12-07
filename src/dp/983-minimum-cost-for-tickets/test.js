348ccee2a180a665ded1bd7f02d07dd9190863c6
RayJune <rayjune.x@gmail.com>
2021-12-07 09:12:12 +0800

    feat(dp): add 983-minimum-cost-for-tickets

---
+++ src/dp/983-minimum-cost-for-tickets/test.js
@@ -0,0 +1,21 @@
+const mincostTickets = require('./index');
+// const mincostTickets = require('./template');
+// const mincostTickets = require('./template-zh');
+
+test('define mincostTickets function', () => {
+    expect(typeof mincostTickets).toBe('function');
+});
+
+test('days = [1, 4, 6, 7, 8, 20], costs = [2, 7, 15]', () => {
+    const days = [1, 4, 6, 7, 8, 20];
+    const costs = [2, 7, 15];
+
+    expect(mincostTickets(days, costs)).toBe(11);
+});
+
+test('days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30, 31], costs = [2, 7, 15]', () => {
+    const days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30, 31];
+    const costs = [2, 7, 15];
+
+    expect(mincostTickets(days, costs)).toBe(17);
+});
