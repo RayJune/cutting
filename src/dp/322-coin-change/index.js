6d28d547df957ac739a26b4cb8e8533c00f732e8
RayJune <rayjune.x@gmail.com>
2021-12-07 11:29:05 +0800

    refactor(dp): little optimize to 322

--- src/dp/322-coin-change/index.js
+++ src/dp/322-coin-change/index.js
@@ -56,9 +56,9 @@ function coinChange(coins, amount) {
     const dp = new Array(amount + 1).fill(Infinity);
 
     dp[0] = 0;
-    for (const num of coins) {
-        for (let j = num; j <= amount; j++) {
-            dp[j] = Math.min(dp[j], dp[j - num] + 1);
+    for (const coin of coins) {
+        for (let i = coin; i <= amount; i++) {
+            dp[i] = Math.min(dp[i], dp[i - coin] + 1);
         }
     }
 
