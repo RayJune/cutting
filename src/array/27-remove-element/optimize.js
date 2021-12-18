e392720d4c457682023fd456be32f7c9e82b2af7
RayJune <rayjune.x@gmail.com>
2021-12-18 14:49:52 +0800

    refactor(array): optimize 27-remove-element

--- src/array/27-remove-element/optimize.js
+++ src/array/27-remove-element/optimize.js
@@ -14,7 +14,7 @@
  * Output: 2, nums = [2, 2, _, _]
  *
  * Example 2:
- * Input: [0, 1, 2, 2, 3, 0, 4, 2], val = 2
+ * Input: nums = [0, 1, 2, 2, 3, 0, 4, 2], val = 2
  * Output: 5, nums = [0, 1, 4, 0, 3, _, _, _]
  *
  * Constraints:
@@ -36,11 +36,7 @@
  * @param {number} val
  * @returns {number}
  */
- function removeElement(nums, val) {
-    if (nums.length === 0) {
-        return 0;
-    }
-
+function removeElement(nums, val) {
     let left = 0;
     let right = nums.length;
 
--- src/array/27-remove-element/template-zh.js
+++ src/array/27-remove-element/template-zh.js
@@ -12,7 +12,7 @@
  * Output: 2, nums = [2, 2, _, _]
  *
  * Example 2:
- * Input: [0, 1, 2, 2, 3, 0, 4, 2], val = 2
+ * Input: nums = [0, 1, 2, 2, 3, 0, 4, 2], val = 2
  * Output: 5, nums = [0, 1, 4, 0, 3, _, _, _]
  *
  * Constraints:
