a8769f9029a4885fef46952e51df5241f0d44c3e
RayJune <rayjune.x@gmail.com>
2021-12-19 13:21:42 +0800

    test(array): simplify 31's test description

--- src/array/31-next-permutation/test.js
+++ src/array/31-next-permutation/test.js
@@ -6,49 +6,49 @@ test('define permute function', () => {
     expect(typeof nextPermutation).toBe('function');
 });
 
-test('input nums = [1]', () => {
+test('nums = [1]', () => {
     const nums = [1];
 
     nextPermutation(nums);
     expect(nums).toEqual([1]);
 });
 
-test('input nums = [2, 1]', () => {
+test('nums = [2, 1]', () => {
     const nums = [2, 1];
 
     nextPermutation(nums);
     expect(nums).toEqual([1, 2]);
 });
 
-test('input nums = [1, 2, 3]', () => {
+test('nums = [1, 2, 3]', () => {
     const nums = [1, 2, 3];
 
     nextPermutation(nums);
     expect(nums).toEqual([1, 3, 2]);
 });
 
-test('input nums = [3, 2, 1]', () => {
+test('nums = [3, 2, 1]', () => {
     const nums = [3, 2, 1];
 
     nextPermutation(nums);
     expect(nums).toEqual([1, 2, 3]);
 });
 
-test('input nums = [1, 1, 5]', () => {
+test('nums = [1, 1, 5]', () => {
     const nums = [1, 1, 5];
 
     nextPermutation(nums);
     expect(nums).toEqual([1, 5, 1]);
 });
 
-test('input nums = [1, 2, 3, 4]', () => {
+test('nums = [1, 2, 3, 4]', () => {
     const nums = [1, 2, 3, 4];
 
     nextPermutation(nums);
     expect(nums).toEqual([1, 2, 4, 3]);
 });
 
-test('input nums = [1, 2, 3, 2, 1]', () => {
+test('nums = [1, 2, 3, 2, 1]', () => {
     const nums = [1, 2, 3, 2, 1];
 
     nextPermutation(nums);
