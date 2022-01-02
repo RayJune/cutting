1bdce41b22b20f4b370742602d8a448adc2639ae
RayJune <rayjune.x@gmail.com>
2022-01-02 11:27:43 +0800

    feat(array): optimize 16-3sum-closet and move it to two-pointers

--- src/array/two-pointers/16-3sum-closest/index.js
+++ src/array/two-pointers/16-3sum-closest/index.js
@@ -1,9 +1,11 @@
 /*
+ * 16. 3Sum Closest
+ *
  * Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+ *
  * Return the sum of the three integers.
  *
  * You may assume that each input would have exactly one solution.
- *
  *
  * Example 1:
  * Input: nums = [-1, 2, 1, -4], target = 1
@@ -11,23 +13,21 @@
  * Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
  *
  * Example 2:
- * Input: nums = [0,0,0], target = 1
+ * Input: nums = [0, 0, 0], target = 1
  * Output: 0
  *
  * Constraints:
- *
  * 3 <= nums.length <= 1000
  * -1000 <= nums[i] <= 1000
- * -10^4 <= target <= 10^4
+ * (-10) ** 4 <= target <= 10 ** 4
  *
  * https://leetcode.com/problems/3sum-closest/
- *
 */
 
 /**
- * 排序 + 双指针
+ * Two Pointers + Sort, 注意对重复值的处理
  *
- * Time Complexity: O(n^2) = for 循环 O(n) * for 循环 O(n) + 排序 O(n*log(n))
+ * Time Complexity: O(n ** 2) = 遍历次数 O(n ** 2) + 排序 O(n * log(n))
  * Space complexity: O(log(n)) = 排序
  * Auxiliary complexity: O(log(n)) = 排序
  *
@@ -36,29 +36,29 @@
  * @returns {number}
  */
 function threeSumClosest(nums, target) {
+    let closet = Infinity;
     const len = nums.length;
-    let gap = Infinity;
 
     nums.sort((a, b) => a - b);
     for (let i = 0; i < len; i++) {
-        let small = i + 1;
-        let big = len - 1;
+        let left = i + 1;
+        let right = len - 1;
 
-        while (small < big) {
-            const newGap = nums[i] + nums[small] + nums[big] - target;
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
 
-            if (Math.abs(newGap) < Math.abs(gap)) {
-                gap = newGap;
+            if (Math.abs(sum - target) < Math.abs(closet - target)) {
+                closet = sum;
             }
-            if (newGap > 0) {
-                big -= 1;
-                while (nums[big] === nums[big + 1]) {
-                    big -= 1;
+            if (sum > target) {
+                right -= 1;
+                while (nums[right] === nums[right + 1]) {
+                    right -= 1;
                 }
-            } else if (newGap < 0) {
-                small += 1;
-                while (nums[small] === nums[small - 1]) {
-                    small += 1;
+            } else if (sum < target) {
+                left += 1;
+                while (nums[left] === nums[left - 1]) {
+                    left += 1;
                 }
             } else {
                 return target;
@@ -66,7 +66,7 @@ function threeSumClosest(nums, target) {
         }
     }
 
-    return gap + target;
+    return closet;
 }
 
 module.exports = threeSumClosest;
--- src/array/two-pointers/16-3sum-closest/template-zh.js
+++ src/array/two-pointers/16-3sum-closest/template-zh.js
@@ -1,6 +1,9 @@
 /*
- * 给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，
- * 使得它们的和与 target 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+ * 16. 最接近的三数之和
+ *
+ * 给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。
+ * 返回这三个数的和。
+ * 假定每组输入只存在唯一答案。
  *
  * Example 1:
  * Input: nums = [-1, 2, 1, -4], target = 1
@@ -8,17 +11,16 @@
  * Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
  *
  * Example 2:
- * Input: nums = [0,0,0], target = 1
+ * Input: nums = [0, 0, 0], target = 1
  * Output: 0
  *
  * Constraints:
  *
  * 3 <= nums.length <= 1000
  * -1000 <= nums[i] <= 1000
- * -10^4 <= target <= 10^4
+ * (-10) ** 4 <= target <= 10 ** 4
  *
  * https://leetcode-cn.com/problems/3sum-closest/
- *
 */
 
 module.exports = threeSumClosest;
