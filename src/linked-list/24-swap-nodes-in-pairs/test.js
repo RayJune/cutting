33c15caf2b6f1967ca9c95c94acda05ed73bf916
RayJune <rayjune.x@gmail.com>
2022-01-11 08:04:14 +0800

    test(linked-list): fix 24-swap-nodes-in-pairs's test case

--- src/linked-list/24-swap-nodes-in-pairs/test.js
+++ src/linked-list/24-swap-nodes-in-pairs/test.js
@@ -10,14 +10,12 @@ test('define swapPairs function', () => {
 test('head = []', () => {
     const head = buildListNode([]);
 
-    expect(swapPairs(head)).toBe(head);
     expect(swapPairs(head)).toEqual(head);
 });
 
 test('head = [1]', () => {
     const head = buildListNode([1]);
 
-    expect(swapPairs(head)).toBe(head);
     expect(swapPairs(head)).toEqual(head);
 });
 
@@ -25,6 +23,5 @@ test('head = [1, 2, 3, 4]', () => {
     const head = buildListNode([1, 2, 3, 4]);
     const newHead = buildListNode([2, 1, 4, 3]);
 
-    expect(swapPairs(head)).toBe(head);
     expect(swapPairs(head)).toEqual(newHead);
 });
