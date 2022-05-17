67120c810aed004019e438580818a94405575304
RayJune <rayjune.x@gmail.com>
2022-05-17 15:38:57 +0800

    refactor(array): optimize prefix-sum/525 with replacing for loop with .forEach method

--- src/array/prefix-sum/525-contiguous-array/index.js
+++ src/array/prefix-sum/525-contiguous-array/index.js
@@ -12,7 +12,6 @@
  * Output: 2
  *
  * Constraints:
- *
  * 1 <= nums.length <= 10 ** 5
  * nums[i] is either 0 or 1.
  *
@@ -22,7 +21,7 @@
 /**
  * Prefix Sum，把 0 视作 -1，把原问题转换为求最长的连续子数组，其元素和为 0
  *
- * Time Complexity: O(n) = for 循环次数
+ * Time Complexity: O(n) = .forEach 遍历次数
  * Space complexity: O(n) = prefixSum 长度
  * Auxiliary complexity: O(n) = prefixSum 长度
  * 其中 n 是数组的长度
@@ -32,18 +31,17 @@
  */
 function findMaxLength(nums) {
     const prefixSum = new Map([[0, -1]]);
-    const len = nums.length;
     let sum = 0;
     let maxLength = 0;
 
-    for (let i = 0; i < len; i++) {
-        sum += (nums[i] === 1 ? 1 : -1);
+    nums.forEach((num, i) => {
+        sum += (num === 1 ? 1 : -1);
         if (prefixSum.has(sum)) {
             maxLength = Math.max(maxLength, i - prefixSum.get(sum));
         } else {
             prefixSum.set(sum, i);
         }
-    }
+    });
 
     return maxLength;
 }
--- src/array/prefix-sum/525-contiguous-array/template-zh.js
+++ src/array/prefix-sum/525-contiguous-array/template-zh.js
@@ -12,7 +12,6 @@
  * Output: 2
  *
  * Constraints:
- *
  * 1 <= nums.length <= 10 ** 5
  * nums[i] is either 0 or 1.
  *
